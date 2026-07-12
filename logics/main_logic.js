331421809150d9ab5efb30eb3c0cbb257c385b60
Ishwor09 <139609758+Ishwor09@users.noreply.github.com>
2026-07-12 20:28:53 +0545

    Update main_logic.js

--- logics/main_logic.js
+++ logics/main_logic.js
@@ -1,35 +1,8 @@
 
-const info_notes = [
-`<div class="more_info">
-    <div style="display: flex; width: 100%; text-align: right; justify-content: flex-end;">
-      <button id="cross" onclick="hideMoreInfo()" style="justify-self: right;">❌</button>
-    </div>
-    <img class="main_img" src="https://scontent.fpkr3-1.fna.fbcdn.net/v/t39.30808-6/745411731_122110637577363771_8138851320240399916_n.jpg?stp=dst-jpg_tt6&cstp=mx2048x1148&ctp=s2048x1148&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHX27BkwF2N2-RKXPIIKEAhSFLYdr4fZtVIUth2vh9m1Tsf2lrVcdoUYLQ5PK7bccDpeyzTY8pv-H8fE-FiK_UC&_nc_ohc=W65ihX-fs0gQ7kNvwHsNvVe&_nc_oc=AdpB51BFzP7Fbp15hlfx7cUCudj1D2DvYFtcXPZhm6v8IcAzHOnIa204rLw6r8fEn9g33qdiQRWZfB4iM5pid1CA&_nc_zt=23&_nc_ht=scontent.fpkr3-1.fna&_nc_gid=yAM2KxAqFEhJqa8Nl1lKwA&_nc_ss=7b2a8&oh=00_AQAvQVd90Oaokk_jhMdKXt37zsGItfDTLiNGWHdclNKXZQ&oe=6A595E51" alt="">
-    <img class="main_img" src="https://scontent.fpkr3-1.fna.fbcdn.net/v/t39.30808-6/746503001_122110637481363771_3476711948870498917_n.jpg?stp=dst-jpg_tt6&cstp=mx2048x2048&ctp=s2048x2048&_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeF71zYrsR0TyxYq3fFtmmtRp2BmeOKA326nYGZ44oDfbn56SxhvIjeEFpWqml6S4cYbg-iY8rCY-y0xFSWswG-Y&_nc_ohc=PwZccRqSuUIQ7kNvwHRoAYD&_nc_oc=Adr3t749RY8b_yRdqjeZNdbyrvsKMqTCBLPadQsidvmI3duIytMfrxa1O52dKt_1m3tcwoO3sNK96W_XVd1D1k2l&_nc_zt=23&_nc_ht=scontent.fpkr3-1.fna&_nc_gid=W0BEicVsnXvDAfJcV8tPdA&_nc_ss=7b2a8&oh=00_AQAa_Y4lD7woLXaJSpEonnSikYPTHoKb0dPSlfVhZDVFFQ&oe=6A595811" alt="">
-    <img class="main_img" src="https://scontent.fpkr3-1.fna.fbcdn.net/v/t39.30808-6/743132100_122110637457363771_3084712289577684976_n.jpg?stp=dst-jpg_tt6&cstp=mx2048x2047&ctp=s2048x2047&_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFy4E17dG6uVRlfm6BwWL6jT9uEVhaWFKhP24RWFpYUqNPajGSUmzCIduBpRV6frb9mKL2pSRVBVmqN0Ly6cJ0G&_nc_ohc=yptz9agakVQQ7kNvwFXI7wZ&_nc_oc=AdqD5NCYOBWWJkevcZsflG0J7FWedLoKP34ZX9zO8DCOUD7e_XAR9zrqCz3ng1JBT52eczPjCKB3_9nFEdHgai_4&_nc_zt=23&_nc_ht=scontent.fpkr3-1.fna&_nc_gid=eMKJXssCYzl06o_Hr8zjgg&_nc_ss=7b2a8&oh=00_AQBmj0X3u3Etgm-JB1vgzet7vLv5rSIaFvH0Hwz4vqRSkw&oe=6A5958BC" alt="">
-    <div id="texts">
-      <h4><b>SUK:</b> 45</h4>
-      <h4><b>Chip:</b> Atmega328p</h4>
-      <p class="item_infos">Arduino is an open-source electronics platform built around easy-to-use hardware and software, perfect for beginners and hobbyists to create interactive projects. It uses microcontroller boards (like the Arduino Uno) that you program with simple code to control sensors, motors, LEDs, and more.</p>
-    </div></div>`,
-
-    `<div class="more_info">
-    <div style="display: flex; width: 100%; text-align: right; justify-content: flex-end;">
-      <button id="cross" onclick="hideMoreInfo()" style="justify-self: right;">❌</button>
-    </div><img class="main_img" src="https://tse3.mm.bing.net/th/id/OIP.gi9HogbqgVIC110gw5EzLwHaHa?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" alt="">
-   <div id="texts">
-      <h4><b>SUK:</b> 35</h4>
-      <h4><b>Chip:</b> Relay Module</h4>
-      <p class="item_infos">Arduino is an open-source electronics platform built around easy-to-use hardware and software, perfect for beginners and hobbyists to create interactive projects. It uses microcontroller boards (like the Arduino Uno) that you program with simple code to control sensors, motors, LEDs, and more.</p>
-        <p class="item_infos">Arduino is an open-source electronics platform built around easy-to-use hardware and software, perfect for beginners and hobbyists to create interactive projects. It uses microcontroller boards (like the Arduino Uno) that you program with simple code to control sensors, motors, LEDs, and more.</p>
-                <p class="item_infos">Arduino is an open-source electronics platform built around easy-to-use hardware and software, perfect for beginners and hobbyists to create interactive projects. It uses microcontroller boards (like the Arduino Uno) that you program with simple code to control sensors, motors, LEDs, and more.</p>
-
-      </div></div>`
-
-];
 
 
 function product(index){
+  checkNet();
     document.body.style.overflow = "hidden";
     document.querySelector('#more_info_id').style = "display: block;";
      document.querySelector('#more_info_id').innerHTML = info_notes[index];
@@ -252,6 +225,12 @@ function toTitleCase(str) {
     .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // capitalize first letter
     .join(" "); // join back
 }
- 
-
-
+ function checkNet() {
+  if (navigator.onLine) {
+    console.log("✅ You are online");
+    return true;
+  } else {
+    console.log("❌ You are offline");
+    return false;
+  }
+}
