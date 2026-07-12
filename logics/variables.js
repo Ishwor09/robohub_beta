942fb2066047332f4c261de9d6a1f3cd8552f145
Ishwor09 <139609758+Ishwor09@users.noreply.github.com>
2026-07-12 17:59:20 +0545

    Add variables.js

---
+++ logics/variables.js
@@ -0,0 +1,32 @@
+
+let cart_dats = [];
+var cart_deletNum = 0;
+let orderStatus_diloc = [
+  "pending",
+  "confirmed & shipped",
+  "delivered",
+  "cancelled",
+  "refunded",
+  "completed",
+  "on_hold",
+  "failed"
+];
+
+let Numbers = [];
+let items_name = [];
+let items_suk = [];
+let priceing = [];
+let total_cost = 0;
+
+
+
+var delivery_fee = 150;
+let buyerName = "";
+let buyerEmail = "";  
+let buyerPhone = "";  
+let buyerDist = "";  
+let buyerLocation = "";  
+let buyerLocationLink = "";  
+let buyerPay = "";  
+let grandTotal = 0;
+let orderStart =orderStatus_diloc[0];
