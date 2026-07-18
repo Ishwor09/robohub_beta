927c37aac4fb557b561da861047867417530996f
Ishwor09 <139609758+Ishwor09@users.noreply.github.com>
2026-07-18 16:25:32 +0545

    Update load_page.js

--- logics/load_page.js
+++ logics/load_page.js
@@ -1,20 +1,20 @@
 
-window.addEventListener("DOMContentLoaded", () => {
-  fetch("bag.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("bag").innerHTML = data;
-    })
-    .catch(error => console.error("Error loading file:", error));
-});
-window.addEventListener("DOMContentLoaded", () => {
-  fetch("kits.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("kits").innerHTML = data;
-    })
-    .catch(error => console.error("Error loading file:", error));
-});
+// window.addEventListener("DOMContentLoaded", () => {
+//   fetch("bag.html")
+//     .then(response => response.text())
+//     .then(data => {
+//       document.getElementById("bag").innerHTML = data;
+//     })
+//     .catch(error => console.error("Error loading file:", error));
+// // });
+// window.addEventListener("DOMContentLoaded", () => {
+//   fetch("kits.html")
+//     .then(response => response.text())
+//     .then(data => {
+//       document.getElementById("kits").innerHTML = data;
+//     })
+//     .catch(error => console.error("Error loading file:", error));
+// });
 
 window.addEventListener("DOMContentLoaded", () => {
   fetch("disctict.html")
